6cb8f734214498f5064fba21e41a960ee0b7115d
Daniel Tsui <tsui.daniel@gmail.com>
2016-06-30 11:13:17 +1000

    home review

---
+++ 01-javascript/objects/objects.js
@@ -0,0 +1,17 @@
+console.log('objects');
+
+var student = {
+  'full name': 'mj',
+  age: 21
+};
+
+var wdi8 = {
+  'class name': 'code_dependent',
+  awesome: true,
+  studentCount: 10,
+  students: ['tad','dean']
+}
+
+// var students = ['tad','dean','']
+
+var languages = ['js','ruby','scala','php','basic'];
